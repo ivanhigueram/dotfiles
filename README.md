ef2d3f55d0d51d45b4b32bfa7b5bb8c7508116f0
ivanhigueram <ivanhigueram@gmail.com>
2017-10-31 01:10:21 -0500

    Changes to .sh file and new Anaconda version

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# dotfiles to set up a developer environment in OSX
+### Configuration dotfiles to set up a developer environment in OSX
+
+This configuration files will make more easy the process of format the OS or configure new Apple machines. The scripts install basic development software (Python, R, NodeJS) and tools (Vim, GNU Utils, etc.) 
+
+#### To-do
+ 1. Add .dotfiles (.zsh, .bash-profile, .vimrc) and comment them
+ 2. Create symlinks in a new .sh file3
+ 3. Create a Github, SSH, etc. configuration files (creds) 
--- initiate.sh
+++ initiate.sh
@@ -1,12 +1,26 @@
 #!/usr/bin/env bash
+
+#Install Apple CLT (no heavy-ass XCode)
 echo Installing required applications - Command Line Tools
 sudo xcode-select --install
+
+#Install Homebrew
 echo Installing homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-echo Install Homebrew formulas using the Brewfile
 
+#Install brew formulas using Brewfile
+echo Install Homebrew formulas using the Brewfile
+brew bundle Brewfile
+
+#Install zsh bindings and autocomplete
 echo Install oh-my-zsh and set-up iTerm2
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"i
+
+#Download/Install Anaconda
+echo Install Python - Anaconda Python 3.6
+wget https://repo.continuum.io/archive/Anaconda3-5.0.1-MacOSX-x86_64.sh
+bash Anaconda3-5.0.1-MacOSX-x86_64.sh
+rm ~/Miniconda3-latest-MacOSX-x86_64.sh
 
 #Clean outdated version in the Cellar (Homebrew library)
 brew cleanup
